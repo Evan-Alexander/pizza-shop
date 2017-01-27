75e061d5390e81b8fb5f8f82ebde392b1ac4ac6c
Jason Brown <jasontbrown99@gmail.com>
2017-01-27 12:54:25 -0800

    create arrays for each big, meat, veggies

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,42 @@
-function Pizza(big, meat, veggies) {
-  this.big = size;
-  this.meat = meat;
-  this.veggies = veggies;
+var big = {
+  name: "small", price: 12,
+  name: "medium", price: 14,
+  name: "large", price: 16,
 }
 
-// Pizza.prototype.makePizza() {
-//   return [this.big, this.meat, this.veggies];
-// }
+var meat = {
+  name: "sausage", price: 2,
+  name: "prosciutto", price: 2,
+  name: "pepperoni", price: 2,
+  name: "canadian bacon", price: 2,
+}
+
+var veggies = {
+  name: "roasted garlic", price: 1,
+  name: "arugula", price: 1,
+  name: "Spanish olives", price: 1,
+  name: "canadian bacon", price: 1,
+  name: "artichoke hearts", price: 1,
+  name: "red onions", price: 1,
+  name: "jalepenos", price: 1,
+  name: "red onions", price: 1,
+  name: "pineapple", price: 1,
+  name: "mushrooms", price: 1,
+}
+
+
+function Pizza(big, meat, veggies) {
+  this.big = big;
+  this.meat = meat;
+  this.veggies = veggies;
+  this.price = function() {
+    return this.big + this.meat + this.veggies
+  }
+}
+
+Pizza.prototype.makePizza() {
+  return [this.big, this.meat, this.veggies];
+}
 
 
 $(document).ready(function() {
