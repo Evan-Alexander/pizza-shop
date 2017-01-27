bee4cd0a2065c5f79904ff8c1b4a5a11bad221f6
Jason Brown <jasontbrown99@gmail.com>
2017-01-27 14:40:18 -0800

    order displays checked selections for pizza size meat and veggie toppings. Still not displaying price and doesn't like it when I try to embellish the string on the return for the prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var big = {
+var pizzaSize = {
   name: "small", price: 12,
   name: "medium", price: 14,
   name: "large", price: 16,
@@ -25,34 +25,45 @@ var veggies = {
 }
 
 
-function Pizza(big, meat, veggies) {
-  this.big = big;
+function Pizza(pizzaSize, meat, veggies) {
+  this.pizzaSize = pizzaSize;
   this.meat = meat;
   this.veggies = veggies;
-  this.price = function() {
-    return this.big + this.meat + this.veggies
-  }
 }
 
-Pizza.prototype.makePizza() {
-  return [this.big, this.meat, this.veggies];
+Pizza.prototype.makePizza = function() {
+  return this.pizzaSize + " " + this.meat + " " + this.veggies;
 }
 
 
 $(document).ready(function() {
   $("form#order-form").submit(function(event) {
     event.preventDefault();
-    $("input:checkbox[name=size]:checked").each(function(){
-        var getSize = $(this).val();
-        $('#order-info').append(getSize + "<br>");
-      });
-    $("input:checkbox[name=meat]:checked").each(function(){
-      var getMeat = $(this).val();
-      $('#order-info').append(getMeat + "<br>");
-      });
-    $("input:checkbox[name=veg]:checked").each(function(){
-      var getVeggies = $(this).val();
-      $('#order-info').append(getVeggies + "<br>");
-      });
+    var getSize = $("input:checkbox[name=size]:checked").val();
+    var getMeat = $("input:checkbox[name=meat]:checked").val();
+    var getVeggies = $("input:checkbox[name=veg]:checked").val();
+
+    var newPizza = new Pizza(getSize, getMeat, getVeggies);
+
+    $('#order-info').append("<li><span>" + newPizza.makePizza() + "</span></li>");
+
+
+
   });
 });
+
+
+
+
+// $("input:checkbox[name=size]:checked").each(function(){
+//   var getSize = $(this).val();
+//   $('#order-info').append("You have ordered" + getSize + "<br>");
+// });
+// $("input:checkbox[name=meat]:checked").each(function(){
+//   var getMeat = $(this).val();
+//   $('#order-info').append(getMeat + "<br>");
+// });
+// $("input:checkbox[name=veg]:checked").each(function(){
+//   var getVeggies = $(this).val();
+//   $('#order-info').append(getVeggies + "<br>");
+// });
