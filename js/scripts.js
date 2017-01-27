c56d7d768ee22d8e1dc483de86264c28593c5ffa
Jason Brown <jasontbrown99@gmail.com>
2017-01-27 10:26:37 -0800

    I got too complicated too fast.  Back to name fields showing up. when order button pushed

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,38 @@
-// function Pizza(size, meat, veggies) {
-//   this.size = size;
-//   this.meat = meat;
-//   this.veggies = veggies;
-// }
-
+//business logic
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
-  this.address = [];
+  this.addresses = [];
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-function Address(sreet, city, state) {
-  this.street = street;
-  this.city = city;
-  this.state = state;
-}
-
-
+// user interface logic
 $(document).ready(function() {
-  $("form#contact-info").submit(function(event) {
+  $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
 
-    var newContact = new Contact (inputtedFirstName, inputtedLastName);
-
-    // $(".new-address").each(function() {
-    //   var inputtedStreet = $(this).find("input.new-street").val();
-    //   var inputtedCity = $(this).find("input.new-city").val();
-    //   var inputtedState = $(this).find("input.new-state").val();
-    //   var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
-    //   newContact.address.push(newAddress);
-    // });
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
 
-
-    $("#delivery-to").show();
-    $("#delivery-to h2").text(newContact.firstName);
+    $("#show-contact").show();
+    $("#show-contact h2").text(newContact.firstName);
     $(".first-name").text(newContact.firstName);
     $(".last-name").text(newContact.lastName);
 
 
-    // $("input#new-first-name").val("");
-    // $("input#new-last-name").val("");
-    // // $("input#new-street").val("");
-    // // $("input#new-city").val("");
-    // // $("input#new-state").val("");
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
   });
 });
