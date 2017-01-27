c46e86955c03f54c149e0d7a96d3203aa1ad3cba
Jason Brown <jasontbrown99@gmail.com>
2017-01-27 10:00:08 -0800

    name fields now pupulate the your order section when submit button is pushed

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,24 @@
-function Pizza(size, meat, veggies) {
-  this.size = size;
-  this.meat = meat;
-  this.veggies = veggies;
-}
+// function Pizza(size, meat, veggies) {
+//   this.size = size;
+//   this.meat = meat;
+//   this.veggies = veggies;
+// }
 
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.address = [];
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-
-
-
-
-
+function Address(sreet, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
 
 
 $(document).ready(function() {
@@ -29,11 +30,26 @@ $(document).ready(function() {
 
     var newContact = new Contact (inputtedFirstName, inputtedLastName);
 
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-    });
+    // $(".new-address").each(function() {
+    //   var inputtedStreet = $(this).find("input.new-street").val();
+    //   var inputtedCity = $(this).find("input.new-city").val();
+    //   var inputtedState = $(this).find("input.new-state").val();
+    //   var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+    //   newContact.address.push(newAddress);
+    // });
+
+
+
+    $("#delivery-to").show();
+    $("#delivery-to h2").text(newContact.firstName);
+    $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);
+
+
+    // $("input#new-first-name").val("");
+    // $("input#new-last-name").val("");
+    // // $("input#new-street").val("");
+    // // $("input#new-city").val("");
+    // // $("input#new-state").val("");
   });
 });
