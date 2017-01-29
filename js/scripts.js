7d6e68e3cddc027f7a906f322173ca85cddda6fe
Jason Brown <jasontbrown99@gmail.com>
2017-01-28 17:02:11 -0800

    meat and veggie prices now display on webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,30 @@
-var smallPie = { name: "small", price: 12 };
-var mediumPie = { name: "medium", price: 14 };
-var largePie = { name: "large", price: 16 };
+// var smallPie = { name: "small", price: 12 };
+// var mediumPie = { name: "medium", price: 14 };
+// var largePie = { name: "large", price: 16 };
+//
+// var sausage = { name: "sausage", price: 2 };
+// var prosciutto = { name: "prosciutto", price: 2 };
+// var pepperoni = { name: "pepperoni", price: 2 };
+// var canadianBacon = { name: "canadianBacon", price: 2 };
+// var sausage = { name: "sausage", price: 2 };
+//
+// var roastedGarlic = { name: "roastedGarlic", price: 1 };
+// var arugula = { name: "arugula", price: 1 };
+// var SpanishOlives = { name: "olives", price: 1 };
+// var canadianBacon = { name: "canadianBacon", price: 1 };
+// var artichokeHearts = { name: "artichokeHearts", price: 1 };
+// var jalepenos = { name: "jalepenos", price: 1 };
+// var redOnions = { name: "onions", price: 1 };
+// var pineapple = { name: "pineapple", price: 1 };
+// var mushrooms = { name: "mushrooms", price: 1 };
 
-var sausage = { name: "sausage", price: 2 };
-var prosciutto = { name: "prosciutto", price: 2 };
-var pepperoni = { name: "pepperoni", price: 2 };
-var canadianBacon = { name: "canadianBacon", price: 2 };
-var sausage = { name: "sausage", price: 2 };
-
-var roastedGarlic = { name: "roastedGarlic", price: 1 };
-var arugula = { name: "arugula", price: 1 };
-var SpanishOlives = { name: "SpanishOlives", price: 1 };
-var canadianBacon = { name: "canadianBacon", price: 1 };
-var artichokeHearts = { name: "artichokeHearts", price: 1 };
-var jalepenos = { name: "jalepenos", price: 1 };
-var redOnions = { name: "redOnions", price: 1 };
-var pineapple = { name: "pineapple", price: 1 };
-var mushrooms = { name: "mushrooms", price: 1 };
-
+// function PizzaPrice(smallPie, mediumPie, largePie, meatPrice, vegPrice) {
+//   this.smallPrice = 12;
+//   this.mediumPrice = 14;
+//   this.largePrice = 16;
+//   this.meatPrice = 2;
+//   this.vegPrice = 1;
+// }
 
 function Pizza(pizzaSize, meat, veggies) {
   this.pizzaSize = pizzaSize;
@@ -25,21 +32,14 @@ function Pizza(pizzaSize, meat, veggies) {
   this.veggies = veggies;
 }
 
-function PizzaPrice(smallPrice, mediumPrice, largePrice, meatPrice, vegPrice) {
-  this.smallPrice = 12;
-  this.mediumPrice = 14;
-  this.largePrice = 16;
-  this.meatPrice = 2;
-  this.vegPrice = 1;
-}
 
 Pizza.prototype.makePizza = function() {
   return this.pizzaSize + " " + this.meat + " with " + this.veggies;
 }
 
 
-// PizzaPrice.prototype.getPrice = function() {
-//   return this.pizzaSize.price + this.meatPrice + this.vegPrice;
+// Meat.prototype.meatPrice = function() {
+//   return this.meat.length * 2;
 // }
 
 $(document).ready(function() {
@@ -52,6 +52,7 @@ $(document).ready(function() {
     $("input:checkbox[name=meat]:checked").each(function(){
         var getMeat = $(this).val();
         meatToppings.push(getMeat);
+        console.log(meatToppings);
       });
 
     var vegToppings = [];
@@ -61,10 +62,10 @@ $(document).ready(function() {
       });
 
     var newPizza = new Pizza(getSize, meatToppings, vegToppings);
-    // var orderCost = new PizzaPrice(getSize, getMeat, getVeggies)
 
     $('#order-info').append("<li><span>" + newPizza.makePizza() + "</span></li>");
-    // $('#cost').append("<li><span>" + orderCost.getPrice() + "</span></li>");
+
+    $('#cost').append("<li><span>" + "$" + parseInt( (meatToppings.length * 2) + vegToppings.length) + "</span></li>");
 
     });
 });
